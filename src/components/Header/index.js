f08b6b2ddf36c66e4aa18e35abed94bf37790e4e
Vinícius Soares <squimer@gmail.com>
2018-04-03 23:18:53 -0300

    Add landing page header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,33 +1,55 @@
 import React from 'react'
+import styled from 'styled-components'
 import Link from 'gatsby-link'
 
-const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
-    </div>
+import logo from "./gdgdivi-logo.png";
+import logo2x from "./gdgdivi-logo@2x.png";
+import logo3x from "./gdgdivi-logo@3x.png";
+import logo4x from "./gdgdivi-logo@4x.png";
+
+const Nav = styled.nav.attrs({ className: 'navbar' })`
+  box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.1);
+  min-height: 5rem;
+`
+
+const Container = styled.div.attrs({ className: 'container' })``
+
+const Burger = styled.div.attrs({ className: 'navbar-burger burger' })`
+  min-height: 5rem;
+`
+
+const LogoImage = styled.img`
+  height: 28px;
+  width: 218px;
+`
+
+const Brand = () => (
+  <div className='navbar-brand'>
+    <Link className='navbar-item' to="/">
+      <LogoImage
+        src={logo}
+        srcSet={`${logo} 1x, ${logo2x} 2x, ${logo3x} 3x, ${logo4x} 4x`}
+        alt='Google Developer Group Divinópolis' />
+    </Link>
+    <Burger data-target='nav-menu'>
+      <span></span>
+      <span></span>
+      <span></span>
+    </Burger>
   </div>
+)
+
+const Header = () => (
+  <Nav>
+    <Container>
+      <Brand/>
+      <div id='nav-menu' className='navbar-menu navbar-end'>
+        <Link className="navbar-item" to="/">O que fazemos</Link>
+        <Link className="navbar-item" to="/">Equipe</Link>
+        <Link className="navbar-item" to="/">Contato</Link>
+      </div>
+    </Container>
+  </Nav>
 )
 
 export default Header
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,10 +2,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
-import Header from '../components/Header'
-
 import 'bulma';
 import './index.scss'
+
+import Header from '../components/Header'
 
 const TemplateWrapper = ({ children }) => (
   <div>
