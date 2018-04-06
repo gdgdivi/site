69b3e27a899a3997b5234c32536b90159fa296b2
Vinícius Soares <squimer@gmail.com>
2018-04-06 18:57:30 -0300

    Update description and keywords

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -9,8 +9,8 @@ const Index = ({ children }) => (
     <Helmet
       title="Google Developer Group Divinópolis"
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content: 'O Google Developers Group de Divinópolis é um grupo aberto de entusiastas em tecnologia que reune pessoas com interesse em tecnologias da Google para trocar conhecimentos através de encontros, reuniões e eventos. ' },
+        { name: 'keywords', content: 'event, gdg, gde, devfest, google, programming, android, chrome, developers, web, cloud, androiddev, ios, iosdev, firebase, react, angular, vue, analytics, divinopolis, divi, mg, minas, minas gerais, comunidade, community' },
       ]}>
       <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js" />
     </Helmet>
