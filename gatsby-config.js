bf290ae226fa096cc5f9802e39c2fca8cccf3c7a
Vinícius Soares <squimer@gmail.com>
2018-04-10 16:17:35 -0300

    Add manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,22 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Google Developer Group Divinópolis',
   },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-styled-components', 'gatsby-plugin-sass']
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-sass',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'Google Developer Group Divinópolis',
+        short_name: 'GDG Divi',
+        start_url: '/',
+        background_color: '#fff',
+        theme_color: '#4688f1',
+        display: 'minimal-ui',
+        icon: 'favicon.png',
+      },
+    },
+  ]
 };
