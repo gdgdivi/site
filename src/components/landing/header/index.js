d73812e37b5147a36df77b4fa73ddd16cdcd828d
Gustavo Pereira <guhpereira@outlook.com.br>
2018-06-14 16:44:07 -0300

    correção scroll top

--- src/components/landing/header/index.js
+++ src/components/landing/header/index.js
@@ -38,11 +38,10 @@ const Brand = () => (
 
 class Header extends Component {
   componentDidMount() {
- 
       $(".navbar-item").click(function(event){        
           event.preventDefault();
-          $('html,body').animate({scrollTop:$(this.hash).offset().top}, 600);
-     });
+          $('html,body').animate({scrollTop:$(this.hash).offset().top-100}, 600);
+     });  
 
   }
   
